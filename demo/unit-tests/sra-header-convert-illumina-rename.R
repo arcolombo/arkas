e44dcd386364c2769346c90ebdec092c3f104b3f
root <anthonycolombo60@gmail.com>
2016-07-07 16:32:03 -0700

    adding a gwa-FDR rolling vignette.  keeping the sra-header conversion as un-tested rmarkdown notes

--- demo/unit-tests/sra-header-convert-illumina-rename.R
+++ demo/unit-tests/sra-header-convert-illumina-rename.R
@@ -1,7 +1,22 @@
+---
+title: "Arkas: Repetitive Elements Quantification In Much Less Time"
+author:  "Timothy J. Triche, Jr, Anthony R. Colombo, Harold Pimentel"
+output: 
+    html_document:
+      toc: true 
+      number_sections: true
+date: "`r format(Sys.time(), '%d %B, %Y')`"
+---
+          
+#Introduction
+Importing from SRA or ENA has fixed headers and changing these headers to be compatible with illumina fastq uploading header conventions is a trivial but useful task.
+
+```
+
 library(arkas)
 library(arkasData)
 library(nat.utils)
-od<-"~/Documents"
+od<-"~/Documents/"
 fastqPath <-system.file("extdata","fastq/SRA",package="arkasData")
 fastqFile<-dir(fastqPath)
 system(paste0("sudo mkdir ",od,"/illuminaTest"))
@@ -15,3 +30,5 @@ fastqFileUploadToBaseSpace(illuminaDirPath="~/Documents/",illuminafastqFile=dir(
 ,basespaceProject="SIRT1_Chip_Seq",illuminaDirs="illuminaTest",dryRun=TRUE,paired=FALSE)
 
 system(paste0("sudo rm -r ",od,"/illuminaTest"))
+
+```
